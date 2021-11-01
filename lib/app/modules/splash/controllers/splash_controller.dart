907714f355816c89b037efc35e02afc98746cbf4
Sanoj Raja <pimen07860@gmail.com>
2021-11-02 00:59:14 +0530

    Added internet permission & created splash screen.

--- lib/app/modules/splash/controllers/splash_controller.dart
+++ lib/app/modules/splash/controllers/splash_controller.dart
@@ -1,6 +1,9 @@
 import 'package:get/get.dart';
+import 'package:nevina_infotech/app/constants/app_assest.dart';
 
 class SplashController extends GetxController {
+  final logo = AppImages.logo;
+
   @override
   void onInit() {
     super.onInit();
